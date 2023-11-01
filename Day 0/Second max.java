1c1bf620a91cc1997b6341320d4ab40d3d4731cb
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-01 18:30:07 +0530

    Update Second_max.java

--- Day 0/Second max.java
+++ Day 0/Second max.java
@@ -1,1 +1,22 @@
+public class Main
+{
+    static int Second_Max(int[] arr){
+        int firstmax=Integer.MIN_VALUE;
+        int secondmax=Integer.MIN_VALUE;
+        for(int num:arr){
+            if(num>firstmax){
+                secondmax=firstmax;
+                firstmax=num;
+            }
+            else if(num>secondmax && num<firstmax){
+                secondmax=num;
+            }
+        }
+        return secondmax;
+    }
+	public static void main(String[] args) {
+		int[] arr={5,4,3,2,1};
+		System.out.println(Second_Max(arr));
+	}
+}
 
