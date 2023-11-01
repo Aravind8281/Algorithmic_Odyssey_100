ecc5128416033c3198e1b0c66063cd322ad36626
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-01 18:41:19 +0530

    Create Last_occurance.java

---
+++ Day 0/Last_occurance.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    static int Last_occurance(int[] array,int target){
+        int n=array.length;
+        for(int i=n-1;i>=0;i--){
+            if(array[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+	public static void main(String[] args) {
+		int[] array = {5, 2, 7, 8, 2, 3, 4, 2, 9};
+
+        int target = 2;
+        System.out.println(Last_occurance(array, target));
+	}
+}
