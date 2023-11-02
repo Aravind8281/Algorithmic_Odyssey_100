d1cd2ca736a12b5776b31d29bbe3a3b7c5fc82f5
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-02 19:32:52 +0530

    Update one-n.java

--- Day 1/one-n.java
+++ Day 1/one-n.java
@@ -1,1 +1,17 @@
-d
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+		oneN(n);
+	}
+	static void oneN(int n){
+	    if(n<=0){
+	        return;
+	    }
+	    System.out.println(n);
+	    oneN(n-1);
+	}
+}
+
