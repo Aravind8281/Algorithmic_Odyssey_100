29686e296046475621b09659f0a234d5c40f104a
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-03 20:13:18 +0530

    Oops Recall
    Here I know that if we declare public it must in a another java file

---
+++ Day 2/Inheritance.java
@@ -0,0 +1,11 @@
+class Fruit{
+    void print(){
+        System.out.print("Hey its Apple");
+    }
+}
+class Main{
+    public static void main(String[] args){
+        Fruit a=new Fruit();
+        a.print();
+    }
+}
