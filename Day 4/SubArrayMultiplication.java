4ff6e3b3d4b099669a39521c46935971be6eca6a
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-05 21:14:45 +0530

    .

---
+++ Day 4/SubArrayMultiplication.java
@@ -0,0 +1,17 @@
+public class Main {
+    static void Multiplication(int[] arr,int target) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if((arr[i]*arr[j])==target){
+                    System.out.print(i+" "+j);
+                    return;
+                }
+            }
+        }   
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        int target=6;
+        Multiplication(arr,target);
+    }
+}
