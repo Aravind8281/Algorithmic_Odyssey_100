36f73c13db16dc3d9bfcd50f0c6d71bbbf2e89f4
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-06 20:46:10 +0530

    .

---
+++ Day 5/StringReplace.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        StringBuffer obj=new StringBuffer("helloworld");
+        obj.replace(0,1,"a");
+        System.out.print(obj);
+    }
+}
