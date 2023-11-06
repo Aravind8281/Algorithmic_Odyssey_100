126b7fe20a6cb12a381e2181f2aaf410e594092e
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-06 20:52:14 +0530

    .

---
+++ Day 5/Print.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        String first="Hello";
+        System.out.println(first);
+        String name="Av";
+        System.out.print(first+name);
+    }
+}
