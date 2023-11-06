ba5b1dcb6fa8a0cf5fe1815347243e98fe77435d
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-06 20:45:35 +0530

    Create CompareTo.java

---
+++ Day 5/CompareTo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        StringBuffer obj=new StringBuffer("helloworld");
+        StringBuffer obj1=new StringBuffer("helloworld");
+        int value=obj.compareTo(obj1);
+        System.out.print(value);
+    }
+}
