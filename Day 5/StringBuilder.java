aa5d8ec9e4b9d9fe4b16955f8e851dc129a7b5db
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-06 20:38:18 +0530

    .

---
+++ Day 5/StringBuilder.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        StringBuffer obj=new StringBuffer("hello");
+        obj=obj.append("world");
+        System.out.print(obj);
+    }
+}
