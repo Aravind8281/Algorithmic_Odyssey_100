43a390dae5222f7fc933ba77dab9be77407effef
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-11 20:21:39 +0530

    .

---
+++ Day 10/Array_multiples.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void Array_multiples(int num,int length){
+        for(int i=1;i<=length;i++){
+            System.out.print(num*i+" ");
+        }
+    }
+	public static void main(String[] args) {
+		Array_multiples(5,10);
+	}
+}
