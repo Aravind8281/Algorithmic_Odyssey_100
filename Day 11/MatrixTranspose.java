24dc9e4520aa77da6a5ad114efcde271940b8d60
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-12 12:03:54 +0530

    .

---
+++ Day 11/MatrixTranspose.java
@@ -0,0 +1,20 @@
+class Main{
+    static void DiagnalSum(int[][] arr){
+        int[][] transpose=new int[arr[0].length][arr.length];
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                transpose[j][i]=arr[i][j];
+            }
+        }
+        for(int i=0;i<arr[0].length;i++){
+            for(int j=0;j<arr.length;j++){
+                System.out.print(transpose[i][j]+" ");
+            }
+            System.out.println("");
+        }
+    }
+    public static void main(String [] args){
+        int[][] arr={{1,2,3},{4,5,6},{7,8,9}};
+        DiagnalSum(arr);
+    }
+}
