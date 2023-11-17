972d3f9bb281c0afa8987eff7d66d79b0320c390
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-17 20:22:16 +0530

    .

---
+++ Day 16/FindDuplicate.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    static void FindDuplicate(int[] arr) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int i : arr) {
+            freq.put(i, freq.getOrDefault(i, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            if(entry.getValue()>1){
+                System.out.println(entry.getKey());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 6,2, 1, 1};
+        FindDuplicate(arr);
+    }
+}
