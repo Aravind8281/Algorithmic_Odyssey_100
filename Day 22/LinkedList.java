6c13bc7e9ab578adfe2d0d5a739bba2be09e8ba6
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-23 19:41:36 +0530

    .

---
+++ Day 22/LinkedList.java
@@ -0,0 +1,14 @@
+public class Main {
+    private Listnode head;
+
+    static class Listnode {
+        private int data;
+        private Listnode next;
+
+        public Listnode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+}
