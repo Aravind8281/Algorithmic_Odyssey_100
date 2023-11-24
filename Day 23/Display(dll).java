a1c10666c07e60e7dd2989584a5f1e41e1f1777e
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-24 15:08:22 +0530

    .

---
+++ Day 23/Display(dll).java
@@ -0,0 +1,56 @@
+class Main {
+    private static Listnode head;
+    private static Listnode tail;
+    private static int length;
+
+    static class Listnode {
+        private int data;
+        private Listnode next;
+        private Listnode previous;
+
+        public Listnode(int data) {
+            this.data = data;
+            this.next = null;
+            this.previous = null;
+        }
+    }
+
+    public Main() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    public void insertAtEnd(int data) {
+        Listnode newNode = new Listnode(data);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.previous = tail;
+            tail = newNode;
+        }
+
+        length++;
+    }
+
+    public void display() {
+        Listnode current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Main doublyLL = new Main();
+
+        doublyLL.insertAtEnd(1);
+        doublyLL.insertAtEnd(2);
+        doublyLL.insertAtEnd(3);
+        display();
+    }
+}
