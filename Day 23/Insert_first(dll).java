c1d1256541563e0d53de5417698100739b1e46cc
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-24 15:14:39 +0530

    .

---
+++ Day 23/Insert_first(dll).java
@@ -0,0 +1,57 @@
+class Main {
+    private static Listnode head;
+    private static Listnode tail;
+    private static int length;
+
+    static class Listnode {
+        private int data;
+        private Listnode next;
+        private Listnode previous;
+
+        public Listnode(int data) {
+            this.data = data;
+            this.next = null;
+            this.previous = null;
+        }
+    }
+
+    public Main() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    public void insertAtFirst(int data) {
+        Listnode newNode = new Listnode(data);
+    
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.previous = newNode;
+            head = newNode;
+        }
+    
+        length++;
+    }
+
+
+    public static void display() {
+        Listnode current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Main doublyLL = new Main();
+
+        doublyLL.insertAtFirst(3);
+        doublyLL.insertAtFirst(2);
+        doublyLL.insertAtFirst(1);
+        display();
+    }
+}
