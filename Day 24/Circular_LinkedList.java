02784a2035e0e8fd37672c8ad5e8e735d03cfbd3
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-25 13:42:43 +0530

    .

---
+++ Day 24/Circular_LinkedList.java
@@ -0,0 +1,25 @@
+class Main{
+    
+    private static Listnode head;
+    private int length;
+    static class Listnode{
+        private int data;
+        private Listnode next;
+        public Listnode(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    public static void main (String[] args) {
+        Main sll=new Main();
+        head=new Listnode(1);
+        Listnode second=new Listnode(2);
+        Listnode third=new Listnode(3);
+        Listnode fourth=new Listnode(4);
+        head.next=second;
+        second.next=third;
+        third.next=fourth;
+        fourth.next=head;
+    }
+}
