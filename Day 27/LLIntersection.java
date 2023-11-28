d178f11708bbd83973f7d3dab978a0a945b71035
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-28 20:38:12 +0530

    .

---
+++ Day 27/LLIntersection.java
@@ -0,0 +1,47 @@
+public class Main{
+	static class Node{
+		int data;
+		Node next;
+		boolean visited;
+		Node(int d){
+			data = d;
+			next = null;
+			visited = false;
+		}
+	}
+	
+	static boolean IntersectNode(Node head1, Node head2){
+	    Node temp=head1;
+	    while(temp!=null){
+	        temp.visited=true;
+	        temp=temp.next;
+	    }
+	    temp=head2;
+	    while(temp!=null){
+	        if(temp.visited){
+	            return true;
+	        }
+	        else{
+	            temp.visited=true;
+	            temp=temp.next;
+	        }
+	        
+	    }
+	    return false;
+	}
+	
+	public static void main(String[] args){
+		Node head1 = new Node(1);
+		head1.next = new Node(2);
+		head1.next.next = new Node(3);
+		head1.next.next.next = new Node(4);
+		head1.next.next.next.next = new Node(5);
+		head1.next.next.next.next.next = new Node(6);
+		head1.next.next.next.next.next.next = new Node(7);
+		Node head2 = new Node(10);
+		head2.next = new Node(9);
+		head2.next.next = new Node(8);
+		head2.next.next.next = new Node(11);
+		System.out.print(IntersectNode(head1, head2));;
+	}
+}
