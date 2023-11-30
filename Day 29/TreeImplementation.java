2ecbecfd3c0cf36ebaddeefe38c4306c29ae6462
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-11-30 13:20:32 +0530

    .

---
+++ Day 29/TreeImplementation.java
@@ -0,0 +1,33 @@
+public class Tree{
+    private static Treenode root;
+    static class Treenode{
+        private Treenode left;
+        private Treenode right;
+        private int data;
+        public Treenode(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public static void main (String[] args) {
+        Tree tr=new Tree();
+        root=new Treenode(0);
+        Treenode first=new Treenode(1);
+        Treenode I_left=new Treenode(2);
+        Treenode I_right=new Treenode(3);
+        Treenode second=new Treenode(4);
+        Treenode II_left=new Treenode(5);
+        Treenode II_right=new Treenode(6);
+        root.left=first;
+        root.right=second;
+        first.left=I_left;
+        first.right=I_right;
+        second.left=II_left;
+        second.right=II_right;
+    }
+}
+
+#                 0
+#             1        2
+#         3      4  5     6
