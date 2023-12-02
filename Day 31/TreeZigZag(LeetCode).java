c37611062a3ebbe53e8bfda0314ed4636920aadf
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-12-02 20:39:04 +0530

    .

---
+++ Day 31/TreeZigZag(LeetCode).java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+public class Tree {
+    private static Treenode root;
+
+    static class Treenode {
+        private Treenode left;
+        private Treenode right;
+        private int data;
+
+        public Treenode(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static ArrayList<Integer> Zigzag() {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Deque<Treenode> q = new LinkedList<>();
+        q.offer(root);
+
+        boolean reversed = false;
+
+        while (!q.isEmpty()) {
+            int level = q.size();
+            ArrayList<Integer> current = new ArrayList<>();
+
+            for (int i = 0; i < level; i++) {
+                if (!reversed) {
+                    Treenode temp = q.pollFirst();
+                    current.add(temp.data);
+                    if (temp.left != null) {
+                        q.addLast(temp.left);
+                    }
+                    if (temp.right != null) {
+                        q.addLast(temp.right);
+                    }
+                } else {
+                    Treenode temp = q.pollLast();
+                    current.add(temp.data);
+                    if (temp.right != null) {
+                        q.addFirst(temp.right);
+                    }
+                    if (temp.left != null) {
+                        q.addFirst(temp.left);
+                    }
+                }
+            }
+            reversed = !reversed;
+            result.addAll(current);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Tree tr = new Tree();
+        root = new Treenode(1);
+        Treenode first = new Treenode(2);
+        Treenode I_left = new Treenode(3);
+        Treenode I_right = new Treenode(4);
+        Treenode second = new Treenode(5);
+        Treenode II_left = new Treenode(6);
+        Treenode II_right = new Treenode(7);
+
+        root.left = first;
+        root.right = second;
+        first.left = I_left;
+        first.right = I_right;
+        second.left = II_left;
+        second.right = II_right;
+
+        System.out.print(Zigzag());
+    }
+}
