81a56ccc50b88686c7591436aab77f29427e2e28
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-12-14 20:33:24 +0530

    .

---
+++ Day 43/TrieLongestPrefix.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+class Trie {
+    private static Trienode root;
+
+    static class Trienode {
+        private Trienode[] children;
+        private boolean flag;
+
+        public Trienode() {
+            this.children = new Trienode[26];
+            this.flag = false;
+        }
+    }
+
+    static void insert(String S) {
+        if (S == null) {
+            throw new IllegalArgumentException("Invalid Input");
+        }
+
+        S = S.toLowerCase();
+        if (root == null) {
+            root = new Trienode();
+        }
+
+        Trienode current = root;
+
+        for (char c : S.toCharArray()) {
+            int index = c - 'a';
+
+            if (current.children[index] == null) {
+                Trienode node = new Trienode();
+                current.children[index] = node;
+            }
+
+            current = current.children[index];
+        }
+
+        current.flag = true;
+    }
+
+    static boolean search(String s) {
+        s = s.toLowerCase();
+        Trienode current = root;
+
+        for (char c : s.toCharArray()) {
+            int index = c - 'a';
+
+            if (current.children[index] == null) {
+                return false;
+            }
+
+            current = current.children[index];
+        }
+
+        return current.flag;
+    }
+
+    static String findLongestWord() {
+        Trienode current = root;
+        StringBuilder longestWord = new StringBuilder();
+
+        while (current != null && !current.flag) {
+            for (int i = 25; i >= 0; i--) {
+                if (current.children[i] != null) {
+                    longestWord.append((char) ('a' + i));
+                    current = current.children[i];
+                    break;
+                }
+            }
+        }
+
+        return longestWord.toString();
+    }
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("cat");
+        trie.insert("cab");
+        trie.insert("cats");
+
+        System.out.println(trie.findLongestWord());
+    }
+}
