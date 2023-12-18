277b859fde40bbe00e60ca94e80e3fd185540841
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-12-18 19:50:32 +0530

    .

---
+++ Day 47/BinarysearchPeakindex.java
@@ -0,0 +1,31 @@
+// Return point where element start decreasing index
+
+import java.util.*;
+class Main{
+    static void PrintArray(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    static int PeakMountain(int[] arr){
+        int low=0;
+        int high=arr.length-1;
+        int mid=0;
+        while (low < high) { 
+            mid = low + (high - low) / 2; 
+            if(arr[mid]>arr[mid+1]){
+                high=mid;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;  
+    }
+    public static void main (String[] args) {
+        int[] arr={1,2,3,4,5,6,4,3,2};
+        PrintArray(arr);
+        System.out.print(PeakMountain(arr));
+    }
+}
