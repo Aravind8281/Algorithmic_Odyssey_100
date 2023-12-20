2c231750047a0c0f0b67747cab0c34db7f37bad5
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-12-20 20:39:48 +0530

    .

---
+++ Day 49/CyclicsortMissingpositive.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    static void swap(int[] arr, int start, int end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+
+    static int SetMismatch(int[] arr) {
+        List<Integer> result = new ArrayList<>();
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] != j + 1) {
+                return j+1; 
+            }
+        }
+
+        return arr.length+1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 2, 7, 8, 2, 3, 1};
+        System.out.println(MissingSmallestPositive(arr));
+    }
+}
