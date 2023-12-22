01a281d9250b17cc8f2b048948a90a7dd52dbbd4
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-12-22 20:09:19 +0530

    .

--- Day 51/RecursionPalindrome.java
+++ Day 51/RecursionPalindrome.java
@@ -1,1 +1,16 @@
-.
+import java.util.*;
+public class Main
+{   static Boolean Palindrome(String str){
+        if(str.length()==0||str.length()==1){
+            return true;
+        }
+        if(str.charAt(0)==str.charAt(str.length()-1)){
+            return Palindrome(str.substring(1,str.length()-1));
+        }
+        return false;
+    }
+	public static void main(String[] args) {
+		String str="Mal";
+		System.out.print(Palindrome(str));
+	}
+}
