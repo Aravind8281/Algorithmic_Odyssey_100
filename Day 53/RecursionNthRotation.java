5070a79f356a106ffc09fbedf32f630d2f6ec9a8
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-12-24 20:54:32 +0530

    .

--- Day 53/RecursionNthRotation.java
+++ Day 53/RecursionNthRotation.java
@@ -6,19 +6,22 @@ public class Main{
         RotateElements(arr, i + 1);
         arr[i + 1] = arr[i];
     }
-    static void Rotate(int[] arr,int k) {
-        int n=k%arr.length;
-        for(int i=0;i<n;i++){
-            int temp = arr[arr.length - 1];
-            RotateElements(arr, 0);
-            arr[0] = temp;
+    static void Rotate(int[] arr,int k,int count) {
+        if(count==k){
+            return;
         }
+        int temp = arr[arr.length - 1];
+        RotateElements(arr, 0);
+        arr[0] = temp;
+        Rotate(arr,k,count+1);
     }
     public static void main (String[] args) {
         int[] arr={2,3,4,5,6,7};
-        Rotate(arr,3);
-        for(int k=0;k<arr.length;k++){
-            System.out.print(arr[k]+"  ");
+        int k=3;
+        int n=k%arr.length;
+        Rotate(arr,n,0);
+        for(int l=0;l<arr.length;l++){
+            System.out.print(arr[l]+"  ");
         }
     }
 }
