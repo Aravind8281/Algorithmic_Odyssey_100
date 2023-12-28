ccb68ceba384f95921b1faac2c96289f93da4591
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-12-28 20:03:21 +0530

    .

---
+++ Day 57/RecursionMazeProblem.java
@@ -0,0 +1,26 @@
+public class Main
+{
+	public static void main(String[] args) {
+		path("",3,3);
+	}
+	static int count(int r,int c){
+	    if(r==1||c==1){
+	        return 1;
+	    }
+	    int left=count(r-1,c);
+	    int right=count(r,c-1);
+	    return left+right;
+	}
+	static void path(String p,int r,int c){
+	    if(r==1 && c==1){
+	        System.out.println(p);
+	        return;
+	    }
+	    if(r>1){
+	        path(p+"D",r-1,c);
+	    }
+	    if(c>1){
+	        path(p+"R",r,c-1);
+	    }
+	}
+}
