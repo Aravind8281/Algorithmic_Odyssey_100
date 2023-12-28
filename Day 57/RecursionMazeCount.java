d876fe888784e201b91515c36e137a8622bcba35
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2023-12-28 19:56:02 +0530

    .

---
+++ Day 57/RecursionMazeCount.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.print(count(3,3));
+	}
+	static int count(int r,int c){
+	    if(r==1||c==1){
+	        return 1;
+	    }
+	    int left=count(r-1,c);
+	    int right=count(r,c-1);
+	    return left+right;
+	}
+}
