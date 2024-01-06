8d608f08eb8a7b57e8cef65837ea6963ce09f22f
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-06 19:52:50 +0530

    Create DPfibTabulation.java.

---
+++ Day 66/DPfibTabulation.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    static int fibTabulation(int n) {
+        int[] arr = new int[n + 1];
+        Arrays.fill(arr, 0);
+        arr[1] = 1; 
+        for (int i = 0; i < n; i++) {
+            arr[i + 1] += arr[i];
+            if (i + 2 <= n) {
+                arr[i + 2] += arr[i];
+            }
+        }
+        return arr[n];
+    }
+    public static void main(String[] args) {
+        System.out.print(fibTabulation(6));
+    }
+}
