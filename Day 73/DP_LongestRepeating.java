a53f15967c94f56d938e76d083043761f39e5c75
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-13 20:01:15 +0530

    .

--- Day 73/DP_LongestRepeating.java
+++ Day 73/DP_LongestRepeating.java
@@ -10,7 +10,7 @@ public class Main
                 if(i==0||j==0){
                     dp[i][j]=0;
                 }
-                else if(s1[i-1]==s2[j-1] && i!=j){
+                else if(s1[i-1]==s2[j-1] && i!=j){// index of two element must not same so i!=j
                     dp[i][j]=1+dp[i-1][j-1];
                 }
                 else{
