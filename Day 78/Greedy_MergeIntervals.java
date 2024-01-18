93947cba43c84c1f64b9c77c1afd302883246b0e
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-18 20:12:03 +0530

    Create Greedy_MergeIntervals.java

---
+++ Day 78/Greedy_MergeIntervals.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeIntervals {
+    public static int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> result = new ArrayList<>();
+        int[] currentInterval = intervals[0];
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] nextInterval = intervals[i];
+            if (currentInterval[1] >= nextInterval[0]) {
+                currentInterval[1] = Math.max(currentInterval[1], nextInterval[1]);
+            } else {
+                result.add(currentInterval);
+                currentInterval = nextInterval;
+            }
+        }
+
+        result.add(currentInterval);
+
+        return result.toArray(new int[result.size()][]);
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+
+        int[][] mergedIntervals = merge(intervals);
+        for (int[] interval : mergedIntervals) {
+            System.out.println(Arrays.toString(interval));
+        }
+    }
+}
