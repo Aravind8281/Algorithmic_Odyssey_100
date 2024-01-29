a0924bc7c1a42b434f19f844c43131c1ab64adab
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-29 20:00:04 +0530

    .

---
+++ Day 89/ExceptionHandling.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main
+{
+    static int div(int a,int b) throws ArithmeticException{
+        if(b==0) {
+            throw new ArithmeticException("not suitable");
+        }
+        return a/b;
+    }
+	public static void main(String[] args) {
+		int a=10,b=0;
+	    try{
+	        System.out.print(div(a,b));
+	    }catch(Exception e){
+	        System.out.print(e.getMessage());
+	    }
+	}
+}
