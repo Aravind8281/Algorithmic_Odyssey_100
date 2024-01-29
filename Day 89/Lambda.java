17d1576560e51701f51ba7b3a0bc9db166ae6802
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-29 20:13:55 +0530

    .

--- Day 89/Lambda.java
+++ Day 89/Lambda.java
@@ -2,6 +2,6 @@ import java.util.*;
 class Main{
     public static void main (String[] args) {
         Runnable myRunnable=()->System.out.print("Message");
-        myRunnable.run();
+        myRunnable.run(); 
     }
 }
