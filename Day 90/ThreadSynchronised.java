d03372d7de5ae70f4d1d730401392dca5d1480f0
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-30 20:19:46 +0530

    .

---
+++ Day 90/ThreadSynchronised.java
@@ -0,0 +1,31 @@
+class Counter {
+    private int count = 0;
+    public synchronized void increment() {
+        count++;
+    }
+    public synchronized int getCount() {
+        return count;
+    }
+}
+class IncrementThread extends Thread {
+    private Counter counter;
+    public IncrementThread(Counter counter) {
+        this.counter = counter;
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            counter.increment();
+            System.out.println(Thread.currentThread().getName() + " - Count: " + counter.getCount());
+        }
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        IncrementThread thread1 = new IncrementThread(counter);
+        IncrementThread thread2 = new IncrementThread(counter);
+        thread1.start();
+        thread2.start();
+    }
+}
