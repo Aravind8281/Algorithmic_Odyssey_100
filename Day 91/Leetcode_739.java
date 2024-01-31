55e03364ba27677b0819ec1a7325f7e9617fc3a7
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-31 20:23:55 +0530

    .

---
+++ Day 91/Leetcode_739.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int index = stack.pop();
+                result[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        return result;
+    }
+}
