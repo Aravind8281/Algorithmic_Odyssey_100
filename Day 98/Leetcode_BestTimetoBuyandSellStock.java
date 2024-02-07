a7d63f5657ee5dde93dbce39c03cb5d7a168a5f2
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-02-07 11:05:03 +0530

    .

---
+++ Day 98/Leetcode_BestTimetoBuyandSellStock.java
@@ -0,0 +1,23 @@
+class Solution{
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0; 
+        }
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                int currentProfit = prices[i] - minPrice;
+                if (currentProfit > maxProfit) {
+                    maxProfit = currentProfit;
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+}
